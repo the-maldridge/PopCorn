8ab0982cc5580d850e7ffd67374aa3e9f9553c4f
Michael Aldridge <aldridge.mac@gmail.com>
2018-05-05 18:13:41 -0700

    Don't highly nest maps

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -8,18 +8,28 @@ import (
 )
 
 type StatsRepo struct {
-	lastSeen map[string]time.Time
-	packages map[string]int
-	versions map[string]map[string]int
-	xuname   map[string]map[string]int
+	lastSeen       map[string]time.Time
+	packages       map[string]int
+	versions       map[string]map[string]int
+	xuOSName       map[string]int
+	xuKernel       map[string]int
+	xuMach         map[string]int
+	xuCPUInfo      map[string]int
+	xuUpdateStatus map[string]int
+	xuRepoStatus   map[string]int
 }
 
 func New() *StatsRepo {
 	return &StatsRepo{
-		lastSeen: make(map[string]time.Time),
-		packages: make(map[string]int),
-		versions: make(map[string]map[string]int),
-		xuname:   make(map[string]map[string]int),
+		lastSeen:       make(map[string]time.Time),
+		packages:       make(map[string]int),
+		versions:       make(map[string]map[string]int),
+		xuOSName:       make(map[string]int),
+		xuKernel:       make(map[string]int),
+		xuMach:         make(map[string]int),
+		xuCPUInfo:      make(map[string]int),
+		xuUpdateStatus: make(map[string]int),
+		xuRepoStatus:   make(map[string]int),
 	}
 }
 
@@ -36,12 +46,12 @@ func (r *StatsRepo) AddStats(s pb.Stats) {
 
 	if s.GetXUname() != nil {
 		x := s.GetXUname()
-		r.xuname["OSName"][x.GetOSName()]++
-		r.xuname["Kernel"][x.GetKernel()]++
-		r.xuname["Mach"][x.GetMach()]++
-		r.xuname["CPUInfo"][x.GetCPUInfo()]++
-		r.xuname["UpdateStatus"][x.GetUpdateStatus()]++
-		r.xuname["RepoStatus"][x.GetRepoStatus()]++
+		r.xuOSName[x.GetOSName()]++
+		r.xuKernel[x.GetKernel()]++
+		r.xuMach[x.GetMach()]++
+		r.xuCPUInfo[x.GetCPUInfo()]++
+		r.xuUpdateStatus[x.GetUpdateStatus()]++
+		r.xuRepoStatus[x.GetRepoStatus()]++
 	}
 
 	log.Println(r)
