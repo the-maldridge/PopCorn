5c12fed7f4a36d9561664c6bae94889ed9c9a8d2
Michael Aldridge <aldridge.mac@gmail.com>
2020-09-02 21:41:36 -0700

    cmd/popcorn: Use HTTP transport

--- cmd/popcorn/main.go
+++ cmd/popcorn/main.go
@@ -2,83 +2,88 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"crypto/sha256"
-	"flag"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 	"time"
 
-	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc"
+	"github.com/hashicorp/go-hclog"
 
-	pb "github.com/the-maldridge/popcorn/internal/proto"
+	"github.com/the-maldridge/popcorn/pkg/stats"
 )
 
 var (
-	uuidPath = flag.String("uuid_path", "/etc/popcorn/uuid", "Path to the uuid file")
+	appLogger hclog.Logger
+	machineID string
 
-	server = flag.String("server", "localhost", "Server to send stats to")
-	port   = flag.Int("port", 8080, "Port to use on the server")
-
-	statInterval = flag.Duration("interval", 24*time.Hour, "Interval to send stats on")
-
-	machineID = ""
+	backoff = []time.Duration{
+		5 * time.Second,
+		5 * time.Second,
+		5 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+		10 * time.Second,
+		1 * time.Minute,
+		1 * time.Minute,
+		1 * time.Minute,
+	}
 )
 
-func getUUID() string {
-	ID, err := ioutil.ReadFile(*uuidPath)
+func getUUID(path string) (string, error) {
+	ID, err := ioutil.ReadFile(path)
 	if os.IsNotExist(err) {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		ID = make([]byte, 96)
 		r.Read(ID)
-		if err := ioutil.WriteFile(*uuidPath, ID, 0644); err != nil {
-			log.Fatal(err)
+		if err := ioutil.WriteFile(path, ID, 0644); err != nil {
+			return "", err
 		}
 	}
 	h := sha256.Sum256(ID)
-	return fmt.Sprintf("%x", h)
+	return fmt.Sprintf("%x", h), nil
 }
 
-func getPkgs() []*pb.Package {
+func getPkgs() ([]stats.Package, error) {
 	_, err := exec.LookPath("xbps-query")
 	if err != nil {
-		log.Println("xbps-query isn't in $PATH.  Are you sure this is a Void system?")
-		log.Fatal(err)
+		return nil, errors.New("xbps-query isn't in $PATH.  Are you sure this is a Void system?")
 	}
 
 	xbpsQueryCmd := exec.Command("xbps-query", "-m")
 	var out bytes.Buffer
 	xbpsQueryCmd.Stdout = &out
 	if err := xbpsQueryCmd.Run(); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	pkgs := []*pb.Package{}
+	pkgs := []stats.Package{}
 	for _, p := range strings.Split(out.String(), "\n") {
 		parts := strings.Split(p, "-")
-		pkg := pb.Package{
-			Name:    proto.String(strings.Join(parts[:len(parts)-1], "-")),
-			Version: proto.String(parts[len(parts)-1]),
+		pkg := stats.Package{
+			Name:    strings.Join(parts[:len(parts)-1], "-"),
+			Version: parts[len(parts)-1],
 		}
-		if pkg.GetName() == "" {
+		if pkg.Name == "" {
 			continue
 		}
-		pkgs = append(pkgs, &pkg)
+		pkgs = append(pkgs, pkg)
 	}
-	return pkgs
+	return pkgs, nil
 }
 
-func getXUname() *pb.XUname {
+func getXUname() (stats.XUname, error) {
 	var out bytes.Buffer
 	_, err := exec.LookPath("xuname")
 	if err != nil {
-		return nil
+		return stats.XUname{}, err
 	}
 	xunameCmd := exec.Command("xuname")
 	xunameCmd.Stdout = &out
@@ -87,73 +92,84 @@ func getXUname() *pb.XUname {
 	}
 	fields := strings.Fields(out.String())
 
-	return &pb.XUname{
-		OSName:       proto.String(fields[0]),
-		Kernel:       proto.String(fields[1]),
-		Mach:         proto.String(fields[2]),
-		CPUInfo:      proto.String(fields[3]),
-		UpdateStatus: proto.String(fields[4]),
-		RepoStatus:   proto.String(fields[5]),
+	return stats.XUname{
+		OSName:       fields[0],
+		Kernel:       fields[1],
+		Mach:         fields[2],
+		CPUInfo:      fields[3],
+		UpdateStatus: fields[4],
+		RepoStatus:   fields[5],
+	}, nil
+}
+
+func collectAndSendStats() {
+	pkgs, err := getPkgs()
+	if err != nil {
+		appLogger.Error("Could not obtain package list", "error", err)
+		return
 	}
+	xuname, err := getXUname()
+	if err != nil {
+		appLogger.Warn("Could not obtain xuname output", "error", err)
+	}
+	d := stats.Stats{
+		Packages: pkgs,
+		XUname:   xuname,
+	}
+
+	body, err := json.Marshal(d)
+	if err != nil {
+		appLogger.Error("Could not marshal data", "error", err)
+		return
+	}
+
+	c := &http.Client{
+		Timeout: time.Second * 15,
+	}
+
+	req, err := http.NewRequest(http.MethodPost, os.Getenv("STATS_URL"), bytes.NewBuffer(body))
+	if err != nil {
+		appLogger.Error("Could not prepare request", "error", err)
+		return
+	}
+	req.Header.Add("From", machineID)
+	req.Header.Add("Content-type", "application/json")
+
+	for _, td := range backoff {
+		resp, err := c.Do(req)
+		if err != nil {
+			appLogger.Warn("Error while transmitting stats data", "error", err)
+			time.Sleep(td)
+			continue
+		}
+		srvMsg, _ := ioutil.ReadAll(resp.Body)
+		appLogger.Info("Message from server", "message", string(srvMsg))
+		appLogger.Info("Stats updated")
+		return
+	}
+	appLogger.Error("Could not transmit stats, giving up")
 }
 
 func main() {
-	flag.Parse()
-	machineID = getUUID()
-
-	backoff := []time.Duration{
-		5 * time.Second,
-		5 * time.Second,
-		5 * time.Second,
-		10 * time.Second,
-		10 * time.Second,
-		10 * time.Second,
-		1 * time.Minute,
-		1 * time.Minute,
-		1 * time.Minute,
+	llevel := os.Getenv("LOG_LEVEL")
+	if llevel == "" {
+		llevel = "INFO"
 	}
+	appLogger = hclog.New(&hclog.LoggerOptions{
+		Name:  "popcorn",
+		Level: hclog.LevelFromString(llevel),
+	})
 
-	var conn *grpc.ClientConn
+	idPath := os.Getenv("UUID_PATH")
+	if idPath == "" {
+		idPath = "/etc/popcorn/uuid"
+	}
 	var err error
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-
-	for i, d := range backoff {
-		conn, err = grpc.Dial(fmt.Sprintf("%s:%d", *server, *port), opts...)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error connecting: %s\n", err)
-			time.Sleep(d)
-			if i == len(backoff)-1 {
-				fmt.Fprintf(os.Stderr, "Exhausted retry attempts during connect\n")
-				os.Exit(1)
-			}
-			continue
-		}
-		break
-	}
-	defer conn.Close()
-
-	client := pb.NewPopCornClient(conn)
-	for i, d := range backoff {
-		if _, err = client.Ping(context.Background(), &pb.EmptyRequest{}); err != nil {
-			time.Sleep(d)
-			if i == len(backoff)-1 {
-				fmt.Fprintf(os.Stderr, "Exhausted retry attempts during ping\n")
-				os.Exit(1)
-			}
-			fmt.Println("Retrying ping")
-			continue
-		}
-		break
-	}
-
-	q := pb.Stats{
-		HostID: &machineID,
-		Pkgs:   getPkgs(),
-		XUname: getXUname(),
-	}
-
-	_, err = client.Update(context.Background(), &q)
+	machineID, err = getUUID(idPath)
 	if err != nil {
-		log.Fatal(err)
+		appLogger.Error("Could not obtain unique identity", "error", err)
+		os.Exit(1)
 	}
+
+	collectAndSendStats()
 }
